a0f29a27516e51b4d1bb931227b8f421bd9d358f
Coenraad Stijne <coenraad@y7mail.com>
2014-07-26 13:37:33 +0200

    First Commit
    Added Menus to MainForm

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Resource_Enumerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.checkBoxAll = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 190);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(260, 60);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // checkBoxAll
+            // 
+            this.checkBoxAll.AutoSize = true;
+            this.checkBoxAll.Location = new System.Drawing.Point(12, 27);
+            this.checkBoxAll.Name = "checkBoxAll";
+            this.checkBoxAll.Size = new System.Drawing.Size(70, 17);
+            this.checkBoxAll.TabIndex = 4;
+            this.checkBoxAll.Text = "Select All";
+            this.checkBoxAll.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.checkBoxAll);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.CheckBox checkBoxAll;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Resource_Enumerator
+{
+    public partial class Form1 : Form
+    {
+        MenuStrip menuStrip1;
+
+        public Form1()
+        {
+            initializeMenu();
+            InitializeComponent();
+        }
+
+        private void initializeMenu()
+        {
+            menuStrip1 = new MenuStrip();
+            ToolStripMenuItem fileItem = new ToolStripMenuItem("File");
+            ToolStripMenuItem openSubItem = new ToolStripMenuItem("Open", null, openToolStripMenuItem_Click);
+            ToolStripMenuItem recentSubItem = new ToolStripMenuItem("Recent");
+
+            if (!String.IsNullOrEmpty(Properties.Settings.Default.Recent1) ||
+                !String.IsNullOrEmpty(Properties.Settings.Default.Recent2) ||
+                !String.IsNullOrEmpty(Properties.Settings.Default.Recent3))
+            {
+                if (!String.IsNullOrEmpty(Properties.Settings.Default.Recent1))
+                {
+                    ToolStripMenuItem recentSubItem1 = new ToolStripMenuItem(Properties.Settings.Default.Recent1);
+                    recentSubItem.DropDownItems.Add(recentSubItem1);
+                }
+
+                if (!String.IsNullOrEmpty(Properties.Settings.Default.Recent2))
+                {
+                    ToolStripMenuItem recentSubItem2 = new ToolStripMenuItem(Properties.Settings.Default.Recent2);
+                    recentSubItem.DropDownItems.Add(recentSubItem2);
+                }
+
+                if (!String.IsNullOrEmpty(Properties.Settings.Default.Recent3))
+                {
+                    ToolStripMenuItem recentSubItem3 = new ToolStripMenuItem(Properties.Settings.Default.Recent3);
+                    recentSubItem.DropDownItems.Add(recentSubItem3);
+                }
+
+                fileItem.DropDownItems.Add(recentSubItem);
+            }
+
+            fileItem.DropDownItems.Add(openSubItem);
+
+            ToolStripMenuItem viewItem = new ToolStripMenuItem("View");
+            ToolStripMenuItem largeIconItem = new ToolStripMenuItem("Large Icons", null, largeIconToolStripMenuItem_Click);
+            ToolStripMenuItem smallIconItem = new ToolStripMenuItem("Small Icons", null, smallIconToolStripMenuItem_Click);
+            ToolStripMenuItem detailItem = new ToolStripMenuItem("Details", null, detailsToolStripMenuItem_Click);
+            ToolStripMenuItem listItem = new ToolStripMenuItem("List", null, listToolStripMenuItem_Click);
+            ToolStripMenuItem tileItem = new ToolStripMenuItem("Tile", null, tileToolStripMenuItem_Click);
+
+            viewItem.DropDownItems.Add(largeIconItem);
+            viewItem.DropDownItems.Add(smallIconItem);
+            viewItem.DropDownItems.Add(detailItem);
+            viewItem.DropDownItems.Add(listItem);
+            viewItem.DropDownItems.Add(tileItem);
+
+            menuStrip1.Items.Add(fileItem);
+            menuStrip1.Items.Add(viewItem);
+
+            this.Controls.Add(menuStrip1);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                enumerateResources(openFileDialog1.FileName);
+            }
+        }
+
+        private void enumerateResources(string fileName)
+        {
+
+        }
+
+        private void largeIconToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.LargeIcon;
+        }
+
+        private void smallIconToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.SmallIcon;
+        }
+
+        private void detailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+        }
+
+        private void listToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.List;
+        }
+
+        private void tileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.Tile;
+        }
+    }
+}
